b6e0de71b47e738f1a348d82c316e9d44187537a
TaylanCann <taylancanh@gmail.com>
2021-07-22 01:39:20 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,348 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAddCar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtModel = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.cbBrands = new System.Windows.Forms.ComboBox();
+            this.rbGas = new System.Windows.Forms.RadioButton();
+            this.rbDiesel = new System.Windows.Forms.RadioButton();
+            this.rbGasoline = new System.Windows.Forms.RadioButton();
+            this.txtEngineCapacity = new System.Windows.Forms.TextBox();
+            this.txtColor = new System.Windows.Forms.TextBox();
+            this.txtYear = new System.Windows.Forms.TextBox();
+            this.txtKm = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label10 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAddCar
+            // 
+            this.btnAddCar.Location = new System.Drawing.Point(419, 217);
+            this.btnAddCar.Name = "btnAddCar";
+            this.btnAddCar.Size = new System.Drawing.Size(228, 45);
+            this.btnAddCar.TabIndex = 0;
+            this.btnAddCar.Text = "Araba Ekle";
+            this.btnAddCar.UseVisualStyleBackColor = true;
+            this.btnAddCar.Click += new System.EventHandler(this.btnAddCar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(234, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Marka";
+            // 
+            // txtModel
+            // 
+            this.txtModel.Location = new System.Drawing.Point(344, 90);
+            this.txtModel.Name = "txtModel";
+            this.txtModel.Size = new System.Drawing.Size(121, 22);
+            this.txtModel.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(235, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Model";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(234, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Yakıt";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(234, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Motor Hacmi";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(234, 177);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Renk";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(601, 79);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(23, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Yıl";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(601, 163);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(93, 17);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Cep Telefonu";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(601, 135);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(38, 17);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Fiyat";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(601, 107);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(67, 17);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "Kilometre";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(180, 307);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 83);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Düzenle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(748, 307);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 83);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Sil";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // cbBrands
+            // 
+            this.cbBrands.FormattingEnabled = true;
+            this.cbBrands.Location = new System.Drawing.Point(344, 60);
+            this.cbBrands.Name = "cbBrands";
+            this.cbBrands.Size = new System.Drawing.Size(121, 24);
+            this.cbBrands.TabIndex = 20;
+            // 
+            // rbGas
+            // 
+            this.rbGas.AutoSize = true;
+            this.rbGas.Location = new System.Drawing.Point(344, 117);
+            this.rbGas.Name = "rbGas";
+            this.rbGas.Size = new System.Drawing.Size(57, 21);
+            this.rbGas.TabIndex = 21;
+            this.rbGas.TabStop = true;
+            this.rbGas.Text = "LPG";
+            this.rbGas.UseVisualStyleBackColor = true;
+            // 
+            // rbDiesel
+            // 
+            this.rbDiesel.AutoSize = true;
+            this.rbDiesel.Location = new System.Drawing.Point(405, 117);
+            this.rbDiesel.Name = "rbDiesel";
+            this.rbDiesel.Size = new System.Drawing.Size(60, 21);
+            this.rbDiesel.TabIndex = 22;
+            this.rbDiesel.TabStop = true;
+            this.rbDiesel.Text = "Dizel";
+            this.rbDiesel.UseVisualStyleBackColor = true;
+            // 
+            // rbGasoline
+            // 
+            this.rbGasoline.AutoSize = true;
+            this.rbGasoline.Location = new System.Drawing.Point(464, 117);
+            this.rbGasoline.Name = "rbGasoline";
+            this.rbGasoline.Size = new System.Drawing.Size(72, 21);
+            this.rbGasoline.TabIndex = 23;
+            this.rbGasoline.TabStop = true;
+            this.rbGasoline.Text = "Benzin";
+            this.rbGasoline.UseVisualStyleBackColor = true;
+            // 
+            // txtEngineCapacity
+            // 
+            this.txtEngineCapacity.Location = new System.Drawing.Point(344, 144);
+            this.txtEngineCapacity.Name = "txtEngineCapacity";
+            this.txtEngineCapacity.Size = new System.Drawing.Size(121, 22);
+            this.txtEngineCapacity.TabIndex = 24;
+            // 
+            // txtColor
+            // 
+            this.txtColor.Location = new System.Drawing.Point(344, 172);
+            this.txtColor.Name = "txtColor";
+            this.txtColor.Size = new System.Drawing.Size(121, 22);
+            this.txtColor.TabIndex = 25;
+            // 
+            // txtYear
+            // 
+            this.txtYear.Location = new System.Drawing.Point(711, 74);
+            this.txtYear.Name = "txtYear";
+            this.txtYear.Size = new System.Drawing.Size(121, 22);
+            this.txtYear.TabIndex = 26;
+            // 
+            // txtKm
+            // 
+            this.txtKm.Location = new System.Drawing.Point(711, 102);
+            this.txtKm.Name = "txtKm";
+            this.txtKm.Size = new System.Drawing.Size(121, 22);
+            this.txtKm.TabIndex = 27;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(711, 158);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(121, 22);
+            this.txtPhone.TabIndex = 28;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(711, 130);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(121, 22);
+            this.txtPrice.TabIndex = 29;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 396);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1124, 294);
+            this.dataGridView1.TabIndex = 30;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(370, 376);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(329, 17);
+            this.label10.TabIndex = 31;
+            this.label10.Text = "Düzenleme ve Silme işlemleri için listeye çift tıklayın";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1148, 702);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtKm);
+            this.Controls.Add(this.txtYear);
+            this.Controls.Add(this.txtColor);
+            this.Controls.Add(this.txtEngineCapacity);
+            this.Controls.Add(this.rbGasoline);
+            this.Controls.Add(this.rbDiesel);
+            this.Controls.Add(this.rbGas);
+            this.Controls.Add(this.cbBrands);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtModel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAddCar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAddCar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtModel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox cbBrands;
+        private System.Windows.Forms.RadioButton rbGas;
+        private System.Windows.Forms.RadioButton rbDiesel;
+        private System.Windows.Forms.RadioButton rbGasoline;
+        private System.Windows.Forms.TextBox txtEngineCapacity;
+        private System.Windows.Forms.TextBox txtColor;
+        private System.Windows.Forms.TextBox txtYear;
+        private System.Windows.Forms.TextBox txtKm;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        List<string> Brands = new List<string>();
+        List<Cars> SavedCars = new List<Cars>();
+        public class Cars
+        {
+            public string Brand;
+            public string Model;
+            public string Fuel;
+            public double EngineCapasity;
+            public string Color;
+            public string Year;
+            public int Kilometer;
+            public decimal Price;
+            public string PhoneNumber;
+        }
+
+        public void loadBrads()
+        {
+            Brands.Add("Fiat");
+            Brands.Add("Hyundai");
+            Brands.Add("Ford");
+            Brands.Add("Renault");
+            Brands.Add("Volvo");
+            Brands.Add("Mercedes");
+            Brands.Add("BMW");
+        }
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            loadBrads();
+            start();
+
+        }
+
+        public void start()
+        {
+            foreach (var item in Brands)
+            {
+                cbBrands.Items.Add(item);
+            }
+
+            dataGridView1.ColumnCount = 9;
+            dataGridView1.Columns[0].Name = "Marka";
+            dataGridView1.Columns[1].Name = "Model";
+            dataGridView1.Columns[2].Name = "Yakıt";
+            dataGridView1.Columns[3].Name = "Motor Hacmi";
+            dataGridView1.Columns[4].Name = "Renk";
+            dataGridView1.Columns[5].Name = "Yıl";
+            dataGridView1.Columns[6].Name = "Kilometre";
+            dataGridView1.Columns[7].Name = "Fiyat";
+            dataGridView1.Columns[8].Name = "Cep Telefonu";
+
+        }
+
+        private void btnAddCar_Click(object sender, EventArgs e)
+        {
+            string rbCheck = "Seçilmedi";
+            if (rbDiesel.Checked == true)
+            {
+                rbCheck = "Dizel";
+            }
+            else if (rbGas.Checked == true)
+            {
+                rbCheck = "LPG";
+            }
+            else if (rbGasoline.Checked == true)
+            {
+                rbCheck = "Benzin";
+            }
+            else
+            {
+                MessageBox.Show("Lütfen Seçim yapınız");
+            }
+
+            Cars car = new Cars();
+            car.Brand = cbBrands.SelectedItem.ToString();
+            car.Color = txtColor.Text.ToString();
+            car.EngineCapasity = int.Parse(txtEngineCapacity.Text);
+            car.Fuel = rbCheck.ToString();
+            car.Kilometer = int.Parse(txtKm.Text);
+            car.Model = txtModel.Text.ToString();
+            car.PhoneNumber = txtPhone.Text.ToString();
+            car.Price = decimal.Parse(txtPrice.Text);
+            car.Year = txtYear.Text.ToString();
+
+            SavedCars.Add(car);
+
+            Listing();
+        }
+
+        private void Listing()
+        {
+            SavedCars = SavedCars.OrderBy(x => x.Brand).ToList();
+            dataGridView1.Rows.Clear();
+            dataGridView1.Rows.Add(SavedCars.Count);
+
+            for (int i = 0; i < SavedCars.Count; i++)
+            {
+                dataGridView1.Rows[i].Cells[0].Value = SavedCars[i].Brand;
+                dataGridView1.Rows[i].Cells[1].Value = SavedCars[i].Model;
+                dataGridView1.Rows[i].Cells[2].Value = SavedCars[i].Fuel;
+                dataGridView1.Rows[i].Cells[3].Value = SavedCars[i].EngineCapasity;
+                dataGridView1.Rows[i].Cells[4].Value = SavedCars[i].Color;
+                dataGridView1.Rows[i].Cells[5].Value = SavedCars[i].Year;
+                dataGridView1.Rows[i].Cells[6].Value = SavedCars[i].Kilometer;
+                dataGridView1.Rows[i].Cells[7].Value = SavedCars[i].Price;
+                dataGridView1.Rows[i].Cells[8].Value = SavedCars[i].PhoneNumber;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string rbCheck = "Seçilmedi";
+            if (rbDiesel.Checked == true)
+            {
+                rbCheck = "Dizel";
+            }
+            else if (rbGas.Checked == true)
+            {
+                rbCheck = "LPG";
+            }
+            else if (rbGasoline.Checked == true)
+            {
+                rbCheck = "Benzin";
+            }
+            else
+            {
+                MessageBox.Show("Lütfen Seçim yapınız");
+            }
+
+            int selectedIndex = dataGridView1.CurrentCell.RowIndex;
+
+            SavedCars[selectedIndex].Brand = cbBrands.SelectedItem.ToString();
+            SavedCars[selectedIndex].Color = txtColor.Text.ToString();
+            SavedCars[selectedIndex].EngineCapasity = int.Parse(txtEngineCapacity.Text);
+            SavedCars[selectedIndex].Fuel = rbCheck.ToString();
+            SavedCars[selectedIndex].Kilometer = int.Parse(txtKm.Text);
+            SavedCars[selectedIndex].Model = txtModel.Text.ToString();
+            SavedCars[selectedIndex].PhoneNumber = txtPhone.Text.ToString();
+            SavedCars[selectedIndex].Price = decimal.Parse(txtPrice.Text);
+            SavedCars[selectedIndex].Year = txtYear.Text.ToString();
+
+            Listing();
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtModel.Text = SavedCars[e.RowIndex].Model;
+            txtPhone.Text = SavedCars[e.RowIndex].PhoneNumber;
+            txtPrice.Text = SavedCars[e.RowIndex].Price.ToString();
+            txtYear.Text = SavedCars[e.RowIndex].Year;
+            txtKm.Text = SavedCars[e.RowIndex].Kilometer.ToString();
+            txtEngineCapacity.Text = SavedCars[e.RowIndex].EngineCapasity.ToString();
+            txtColor.Text = SavedCars[e.RowIndex].Color.ToString();
+            
+            for (int i = 0; i < cbBrands.Items.Count; i++)
+            {
+                if (cbBrands.Items[i].ToString() == SavedCars[e.RowIndex].Brand)
+                {
+                    cbBrands.SelectedIndex = i;
+                    break;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int selectedIndex = dataGridView1.CurrentCell.RowIndex;
+
+            dataGridView1.Rows.RemoveAt(selectedIndex);
+
+            SavedCars.RemoveAt(selectedIndex);
+
+            dataGridView1.Refresh();
+        }
+    }
+}
